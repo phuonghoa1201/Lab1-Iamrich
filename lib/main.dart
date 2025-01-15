7d5e4e57c39ed1621a0dbbd99adc1dc6e65680b0
phuonghoa2101 <phuonghoa21604@gmail.com>
2025-01-15 21:17:17 +0700

    lab1

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am Rich',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 30, 32, 54),
+        title: const Text('I am Rich', style: TextStyle(color: Colors.pink),),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset('assets/diamond.png'),
+      ),
+    );
+  }
+}
+
+
+
